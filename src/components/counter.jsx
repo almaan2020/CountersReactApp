2a72f6abf77ad50f01788bf2b9fd9f4a0cce45f9
AminRahimzadeh <31921848+almaan2020@users.noreply.github.com>
2021-12-08 10:15:55 +0330

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,21 +2,32 @@ import React, { Component } from "react";
 export default class Counter extends React.Component {
   render() {
     return (
-      <div>
+      <div className="row">
         {this.props.children}
+        <div className="col-1">
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
         <button
           onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
-          Increment
+          +
         </button>
         <button
+            onClick={() => this.props.onDecrement(this.props.counter)}
+            className="btn btn-secondary btn-sm m-2"
+            disabled={this.props.counter.value === 0 ? "disabled" : ""}
+          >
+            -
+          </button>
+        <button
           onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm m-2"
+          className="btn btn-danger btn-sm"
         >
           Delete
         </button>
+        </div>
       </div>
     );
   }
